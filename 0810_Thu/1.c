2480b5e932758faedcdfed761ac2a5efa16835fc
PERSONN <sonth2405@naver.com>
2023-08-15 23:00:54 +0900

    <ADD> 0809_Thu

--- 0810_Thu/1.c
+++ 0810_Thu/1.c
@@ -1,3 +1,28 @@
 // 연습문제 1
 
 // 1부터 n까지의 합 중에서 10000을 넘지 않는 가장 큰 합과 그때의 n 값을 구하는 프로그램
+
+#include <stdio.h>
+
+int main(void){
+    // 합 & n
+
+    int sum = 0;
+    int num;
+    int i;
+
+    for(i=1;i<=5050;i++){
+        printf("%d\n",i);
+        if (sum + i>5050){
+            printf("Break!\n");
+            break;
+        }
+        else{
+            sum = sum+i;
+            num = i;
+            printf("Else\n");
+        }
+    }
+
+    printf("%d %d\n",sum,num);
+}
--- 0810_Thu/2.c
+++ 0810_Thu/2.c
@@ -2,3 +2,18 @@
 
 // 1부터 n까지의 곱 중에서 10000을 넘지 않는 가장 큰 곱과 그 때의 n 값을 구하시오.
 
+#include <stdio.h>
+
+int main(void){
+    // 합 & n
+
+    int sum = 1;
+    int num;
+    int i;
+
+    for(i=1;sum*i<=10000;i++){
+        sum = sum * i;
+    }
+
+    printf("%d %d\n",sum,i-1);
+}
